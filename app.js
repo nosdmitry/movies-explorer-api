87077b33a32029ac7f9b53577119efcbc7b43df1
Dmitry Nosov <nosdmitry@gmail.com>
2021-06-07 21:23:04 +0300

    feat: limiter, cors and helmet

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+const { MONGO_URL, PORT } = require('./config');
 const { errorsHandler } = require('./middlewares/errorsHandler');
 const routes = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { MONGO_URL, PORT } = require('./config');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
 
 const app = express();
-
+app.use(helmet());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+  allowedHeaders: ['Origin', 'Access-Control-Allow-Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+}));
+app.use(limiter);
 app.use(express.json());
 
 app.use(requestLogger);
