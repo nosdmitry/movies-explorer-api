c7430a800a269735293f3c56cd281f5ea8154f45
Dmitry Nosov <nosdmitry@gmail.com>
2021-07-04 22:35:08 +0300

    cors

--- app.js
+++ app.js
@@ -15,9 +15,8 @@ const limiter = rateLimit(limiterConfig);
 const app = express();
 app.use(helmet());
 app.use(cors({
-  origin: 'https://final.nomoredomains.monster',
+  origin: '*',
   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
-  allowedHeaders: ['Origin', 'Access-Control-Allow-Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
   credentials: true,
 }));
 
